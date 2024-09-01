7097fc4ffc6539d274ca208b6d771067754c5a4d
linusrg <l.rueegg@dirageosystems.ch>
2024-09-01 14:36:39 +0200

    add automad

---
+++ automad/automad/src/server/Blocks/Code.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ *                    ....
+ *                  .:   '':.
+ *                  ::::     ':..
+ *                  ::.         ''..
+ *       .:'.. ..':.:::'    . :.   '':.
+ *      :.   ''     ''     '. ::::.. ..:
+ *      ::::.        ..':.. .''':::::  .
+ *      :::::::..    '..::::  :. ::::  :
+ *      ::'':::::::.    ':::.'':.::::  :
+ *      :..   ''::::::....':     ''::  :
+ *      :::::.    ':::::   :     .. '' .
+ *   .''::::::::... ':::.''   ..''  :.''''.
+ *   :..:::'':::::  :::::...:''        :..:
+ *   ::::::. '::::  ::::::::  ..::        .
+ *   ::::::::.::::  ::::::::  :'':.::   .''
+ *   ::: '::::::::.' '':::::  :.' '':  :
+ *   :::   :::::::::..' ::::  ::...'   .
+ *   :::  .::::::::::   ::::  ::::  .:'
+ *    '::'  '':::::::   ::::  : ::  :
+ *              '::::   ::::  :''  .:
+ *               ::::   ::::    ..''
+ *               :::: ..:::: .:''
+ *                 ''''  '''''
+ *
+ *
+ * AUTOMAD
+ *
+ * Copyright (c) 2020-2024 by Marc Anton Dahmen
+ * https://marcdahmen.de
+ *
+ * Licensed under the MIT license.
+ * https://automad.org/license
+ */
+
+namespace Automad\Blocks;
+
+use Automad\Blocks\Utils\Attr;
+use Automad\Core\Automad;
+
+defined('AUTOMAD') or die('Direct access not permitted!');
+
+/**
+ * The code block.
+ *
+ * @author Marc Anton Dahmen
+ * @copyright Copyright (c) 2020-2024 by Marc Anton Dahmen - https://marcdahmen.de
+ * @license MIT license - https://automad.org/license
+ *
+ * @psalm-import-type BlockData from AbstractBlock
+ */
+class Code extends AbstractBlock {
+	/**
+	 * Render a code block.
+	 *
+	 * @param BlockData $block
+	 * @param Automad $Automad
+	 * @return string the rendered HTML
+	 */
+	public static function render(array $block, Automad $Automad): string {
+		$code = htmlspecialchars($block['data']['code']);
+		$lang = 'language-' . ($block['data']['language'] ?? '');
+		$lines = ($block['data']['lineNumbers'] ?? false) ? ' class="line-numbers"' : '';
+		$attr = Attr::render($block['tunes']);
+
+		return <<< HTML
+			<div $attr>
+				<pre $lines><code class="$lang" data-prismjs-copy="Copy">$code</code></pre>
+			</div>
+			HTML;
+	}
+}
